4413a53f12c430322279aa469abb516cb05f50a8
Kensuke Tamura <kazakago@gmail.com>
2022-11-25 08:35:50 +0900

    add publishing script

--- swr/build.gradle.kts
+++ swr/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+    id("signing")
 }
 
 android {
@@ -32,6 +34,12 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.3.2"
     }
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -43,4 +51,58 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.4")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.0")
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("release") {
+            groupId = "com.kazakago.swr.compose"
+            artifactId = "swr-android"
+            version = "0.1.0"
+            pom {
+                name.set("swr-compose")
+                description.set("\"React SWR\" ported for Jetpack Compose")
+                url.set("https://github.com/kazakago/swr-compose")
+                licenses {
+                    license {
+                        name.set("Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+                scm {
+                    connection.set("git:git@github.com:kazakago/swr-compose")
+                    developerConnection.set("git:git@github.com:kazakago/swr-compose")
+                    url.set("https://github.com/kazakago/swr-compose")
+                }
+                developers {
+                    developer {
+                        name.set("kazakago")
+                        email.set("kazakago@gmail.com")
+                        url.set("https://blog.kazakago.com/")
+                    }
+                }
+            }
+            afterEvaluate {
+                from(components["release"])
+            }
+            signing {
+                val keyId = System.getenv("SIGNING_KEY_ID") ?: findProperty("signing.keyId").toString()
+                val secretKey = System.getenv("SIGNING_SECRET_KEY") ?: findProperty("signing.secretKey").toString()
+                val password = System.getenv("SIGNING_PASSWORD") ?: findProperty("signing.password").toString()
+                useInMemoryPgpKeys(keyId, secretKey, password)
+                sign(this@register)
+            }
+            repositories {
+                maven {
+                    val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+                    val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots")
+                    url = if (version.endsWith("-SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+                    credentials {
+                        username = System.getenv("SONATYPE_USERNAME") ?: findProperty("sonatype.username").toString()
+                        password = System.getenv("SONATYPE_PASSWORD") ?: findProperty("sonatype.password").toString()
+                    }
+                }
+            }
+        }
+    }
 }
