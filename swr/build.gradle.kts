67274c9f677dc0aa3a4d297e21272d96fa0b17cd
Kensuke Tamura <kazakago@gmail.com>
2022-12-14 10:37:00 +0900

    update version

--- swr/build.gradle.kts
+++ swr/build.gradle.kts
@@ -69,7 +69,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.kazakago.swr.compose"
             artifactId = "swr-android"
-            version = "0.1.0"
+            version = "0.2.0"
             pom {
                 name.set("swr-compose")
                 description.set("\"React SWR\" ported for Jetpack Compose")
