ed2b684067cb55fcc7faf61403253cabb41ecb4e
Kensuke Tamura <kazakago@gmail.com>
2022-12-20 14:34:15 +0900

    add fetcher option tests

---
+++ swr/src/test/java/com/kazakago/swr/compose/validation/FetcherOptionTest.kt
@@ -0,0 +1,101 @@
+package com.kazakago.swr.compose.validation
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kazakago.swr.compose.config.SWRConfig
+import com.kazakago.swr.compose.internal.SWRGlobalScope
+import com.kazakago.swr.compose.state.SWRState
+import com.kazakago.swr.compose.useSWR
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+public class FetcherOptionTest {
+
+    @get:Rule
+    public val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
+
+    @get:Rule
+    public val composeTestRule2: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
+
+    @Test
+    public fun localFetcher() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            stateList += useSWR(key = key) {
+                fetcher = {
+                    delay(100)
+                    "fetched"
+                }
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(2500)
+        stateList.map { it.data } shouldBe listOf(null, null, "fetched")
+        stateList.map { it.error } shouldBe listOf(null, null, null)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false)
+    }
+
+    @Test
+    public fun savedFetcher() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRState<String, String>>()
+        val stateList2 = mutableListOf<SWRState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            stateList += useSWR(key = key, fetcher = {
+                delay(100)
+                "fetched"
+            })
+            stateList2 += useSWR(key = key)
+        }
+        composeTestRule.mainClock.advanceTimeBy(2500)
+
+        stateList.map { it.data } shouldBe listOf(null, null, "fetched")
+        stateList.map { it.error } shouldBe listOf(null, null, null)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false)
+
+        stateList2.map { it.data } shouldBe listOf(null, null, "fetched")
+        stateList2.map { it.error } shouldBe listOf(null, null, null)
+        stateList2.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList2.map { it.isValidating } shouldBe listOf(false, true, false)
+    }
+
+    @Test
+    public fun globalFetcher() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            SWRConfig(
+                options = {
+                    fetcher = {
+                        delay(100)
+                        "fetched"
+                    }
+                }
+            ) {
+                stateList += useSWR(key = key)
+            }
+        }
+        composeTestRule.mainClock.advanceTimeBy(2500)
+        stateList.map { it.data } shouldBe listOf(null, null, "fetched")
+        stateList.map { it.error } shouldBe listOf(null, null, null)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false)
+    }
+}
