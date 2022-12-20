74188918596f41f0f91e2ad200fb2a65054c3b15
Kensuke Tamura <kazakago@gmail.com>
2022-12-20 23:15:29 +0900

    add OnLoadingSlowOptionTest

---
+++ swr/src/test/java/com/kazakago/swr/compose/validation/OnLoadingSlowOptionTest.kt
@@ -0,0 +1,65 @@
+package com.kazakago.swr.compose.validation
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kazakago.swr.compose.internal.SWRGlobalScope
+import com.kazakago.swr.compose.useSWR
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.time.Duration.Companion.seconds
+
+@RunWith(AndroidJUnit4::class)
+public class OnLoadingSlowOptionTest {
+
+    @get:Rule
+    public val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
+
+    @Test
+    public fun onLoadingSlow() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val keyList = mutableListOf<String>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            useSWR(key = key, fetcher = {
+                delay(5.seconds)
+                "fetched"
+            }) {
+                onLoadingSlow = { key, _ ->
+                    keyList.add(key)
+                }
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(5000)
+        keyList shouldBe listOf(key)
+    }
+
+    @Test
+    public fun onLoadingSlow2() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val keyList = mutableListOf<String>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            useSWR(key = key, fetcher = {
+                delay(2.seconds)
+                "fetched"
+            }) {
+                onLoadingSlow = { key, _ ->
+                    keyList.add(key)
+                }
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(5000)
+        keyList shouldBe emptyList()
+    }
+}
