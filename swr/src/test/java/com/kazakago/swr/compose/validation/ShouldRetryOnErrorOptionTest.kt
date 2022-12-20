415b63397285a59bebba1f94e29d78c04b68264d
Kensuke Tamura <kazakago@gmail.com>
2022-12-20 21:56:51 +0900

    add ShouldRetryOnErrorOptionTest

---
+++ swr/src/test/java/com/kazakago/swr/compose/validation/ShouldRetryOnErrorOptionTest.kt
@@ -0,0 +1,70 @@
+package com.kazakago.swr.compose.validation
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.lifecycle.Lifecycle
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kazakago.swr.compose.DummyException1
+import com.kazakago.swr.compose.internal.SWRGlobalScope
+import com.kazakago.swr.compose.state.SWRState
+import com.kazakago.swr.compose.useSWR
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import kotlin.time.Duration.Companion.seconds
+
+@RunWith(AndroidJUnit4::class)
+public class ShouldRetryOnErrorOptionTest {
+
+    @get:Rule
+    public val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
+
+    @Test
+    public fun withShouldRetryOnError() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            stateList += useSWR(key = key, fetcher = {
+                delay(100)
+                throw DummyException1
+            }) {
+                shouldRetryOnError = true
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(15000)
+        stateList.map { it.data } shouldBe listOf(null, null, null, null, null)
+        stateList.map { it.error } shouldBe listOf(null, null, DummyException1, DummyException1, DummyException1)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false, false, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false, true, false)
+    }
+
+    @Test
+    public fun noShouldRetryOnError() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            stateList += useSWR(key = key, fetcher = {
+                delay(100)
+                throw DummyException1
+            }) {
+                shouldRetryOnError = false
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(15000)
+        stateList.map { it.data } shouldBe listOf(null, null, null)
+        stateList.map { it.error } shouldBe listOf(null, null, DummyException1)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false)
+    }
+}
