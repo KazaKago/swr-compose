d2855ff6a92fc8916d01b4cf6f176c8f3acf4245
Kensuke Tamura <kazakago@gmail.com>
2022-12-20 16:17:36 +0900

    add RevalidateOnFocusOptionTest

--- swr/src/test/java/com/kazakago/swr/compose/validation/RevalidateOnFocusOptionTest.kt
+++ swr/src/test/java/com/kazakago/swr/compose/validation/RevalidateOnFocusOptionTest.kt
@@ -4,27 +4,26 @@ import androidx.activity.ComponentActivity
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.lifecycle.Lifecycle
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.kazakago.swr.compose.internal.SWRGlobalScope
 import com.kazakago.swr.compose.state.SWRState
 import com.kazakago.swr.compose.useSWR
 import io.kotest.matchers.shouldBe
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-public class RevalidateOnMountTest {
+public class RevalidateOnFocusOptionTest {
 
     @get:Rule
     public val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
 
     @Test
-    public fun noRevalidateOnMount() {
+    public fun withRevalidateOnFocus() {
         val key = object {}.javaClass.enclosingMethod?.name
         val stateList = mutableListOf<SWRState<String, String>>()
         composeTestRule.mainClock.autoAdvance = false
@@ -34,43 +33,41 @@ public class RevalidateOnMountTest {
                 delay(100)
                 "fetched"
             }) {
-                revalidateOnMount = false
+                revalidateOnFocus = true
             }
         }
 
         composeTestRule.mainClock.advanceTimeBy(2500)
-        stateList.map { it.data } shouldBe listOf(null)
-        stateList.map { it.error } shouldBe listOf(null)
-        stateList.map { it.isLoading } shouldBe listOf(true)
-        stateList.map { it.isValidating } shouldBe listOf(false)
+        composeTestRule.activityRule.scenario.moveToState(Lifecycle.State.STARTED)
+        composeTestRule.mainClock.advanceTimeBy(5000)
+        composeTestRule.activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
+
+        composeTestRule.mainClock.advanceTimeBy(2500)
+        stateList.map { it.data } shouldBe listOf(null, null, "fetched", "fetched", "fetched")
+        stateList.map { it.error } shouldBe listOf(null, null, null, null, null)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false, false, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false, true, false)
     }
 
     @Test
-    public fun noRevalidateOnMountWithMutate() {
+    public fun noRevalidateOnFocus() {
         val key = object {}.javaClass.enclosingMethod?.name
         val stateList = mutableListOf<SWRState<String, String>>()
-        lateinit var scope: CoroutineScope
         composeTestRule.mainClock.autoAdvance = false
         composeTestRule.setContent {
-            scope = rememberCoroutineScope()
             SWRGlobalScope = rememberCoroutineScope()
             stateList += useSWR(key = key, fetcher = {
                 delay(100)
                 "fetched"
             }) {
-                revalidateOnMount = false
+                revalidateOnFocus = false
             }
         }
 
         composeTestRule.mainClock.advanceTimeBy(2500)
-        stateList.map { it.data } shouldBe listOf(null)
-        stateList.map { it.error } shouldBe listOf(null)
-        stateList.map { it.isLoading } shouldBe listOf(true)
-        stateList.map { it.isValidating } shouldBe listOf(false)
-
-        scope.launch {
-            stateList.last().mutate()
-        }
+        composeTestRule.activityRule.scenario.moveToState(Lifecycle.State.STARTED)
+        composeTestRule.mainClock.advanceTimeBy(5000)
+        composeTestRule.activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
 
         composeTestRule.mainClock.advanceTimeBy(2500)
         stateList.map { it.data } shouldBe listOf(null, null, "fetched")
