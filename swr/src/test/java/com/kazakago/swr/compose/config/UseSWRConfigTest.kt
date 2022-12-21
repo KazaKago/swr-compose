36c32034fe018632ced232327dc6218cb8d7a950
KazaKago <kensuke_tamura@r.recruit.co.jp>
2022-12-21 14:39:57 +0900

    add UseSWRConfigTest

---
+++ swr/src/test/java/com/kazakago/swr/compose/config/UseSWRConfigTest.kt
@@ -0,0 +1,51 @@
+package com.kazakago.swr.compose.config
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kazakago.swr.compose.internal.SWRGlobalScope
+import com.kazakago.swr.compose.state.SWRState
+import com.kazakago.swr.compose.useSWR
+import com.kazakago.swr.compose.useSWRConfig
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+public class UseSWRConfigTest {
+
+    @get:Rule
+    public val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
+
+    @Test
+    public fun mutation() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            stateList += useSWR(key = key, fetcher = {
+                delay(100)
+                "fetched"
+            }) {
+                revalidateOnMount = false
+            }
+            val (mutate, _, _) = useSWRConfig<String, String>()
+            LaunchedEffect(Unit) {
+                mutate(key = key)
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(1000)
+        stateList.map { it.data } shouldBe listOf(null, null, "fetched")
+        stateList.map { it.error } shouldBe listOf(null, null, null)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false)
+    }
+}
