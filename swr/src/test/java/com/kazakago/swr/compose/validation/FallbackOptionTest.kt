fb40d2c524506b737ec4e208430d52d300e98df5
KazaKago <kensuke_tamura@r.recruit.co.jp>
2022-12-21 10:51:29 +0900

    add FallbackOptionTest

--- swr/src/test/java/com/kazakago/swr/compose/validation/FallbackOptionTest.kt
+++ swr/src/test/java/com/kazakago/swr/compose/validation/FallbackOptionTest.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.test.junit4.AndroidComposeTestRule
 import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kazakago.swr.compose.config.SWRConfig
 import com.kazakago.swr.compose.internal.SWRGlobalScope
 import com.kazakago.swr.compose.state.SWRState
 import com.kazakago.swr.compose.useSWR
@@ -16,23 +17,25 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-public class FallbackDataOptionTest {
+public class FallbackOptionTest {
 
     @get:Rule
     public val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
 
     @Test
-    public fun noFallbackData() {
+    public fun noFallback() {
         val key = object {}.javaClass.enclosingMethod?.name
         val stateList = mutableListOf<SWRState<String, String>>()
         composeTestRule.mainClock.autoAdvance = false
         composeTestRule.setContent {
             SWRGlobalScope = rememberCoroutineScope()
-            stateList += useSWR(key = key, fetcher = {
-                delay(100)
-                "fetched"
+            SWRConfig(options = {
+                fallback = emptyMap()
             }) {
-                fallbackData = null
+                stateList += useSWR(key = key, fetcher = {
+                    delay(100)
+                    "fetched"
+                })
             }
         }
 
@@ -44,17 +47,19 @@ public class FallbackDataOptionTest {
     }
 
     @Test
-    public fun withFallbackData() {
+    public fun withFallback() {
         val key = object {}.javaClass.enclosingMethod?.name
         val stateList = mutableListOf<SWRState<String, String>>()
         composeTestRule.mainClock.autoAdvance = false
         composeTestRule.setContent {
             SWRGlobalScope = rememberCoroutineScope()
-            stateList += useSWR(key = key, fetcher = {
-                delay(100)
-                "fetched"
+            SWRConfig(options = {
+                fallback = mapOf<Any, Any>(key!! to "fallback")
             }) {
-                fallbackData = "fallback"
+                stateList += useSWR(key = key, fetcher = {
+                    delay(100)
+                    "fetched"
+                })
             }
         }
 
@@ -66,18 +71,21 @@ public class FallbackDataOptionTest {
     }
 
     @Test
-    public fun withFallbackDataAndRevalidateOnMount() {
+    public fun withFallbackAndRevalidateOnMount() {
         val key = object {}.javaClass.enclosingMethod?.name
         val stateList = mutableListOf<SWRState<String, String>>()
         composeTestRule.mainClock.autoAdvance = false
         composeTestRule.setContent {
             SWRGlobalScope = rememberCoroutineScope()
-            stateList += useSWR(key = key, fetcher = {
-                delay(100)
-                "fetched"
+            SWRConfig(options = {
+                fallback = mapOf<Any, Any>(key!! to "fallback")
             }) {
-                fallbackData = "fallback"
-                revalidateOnMount = true
+                stateList += useSWR(key = key, fetcher = {
+                    delay(100)
+                    "fetched"
+                }) {
+                    revalidateOnMount = true
+                }
             }
         }
 
