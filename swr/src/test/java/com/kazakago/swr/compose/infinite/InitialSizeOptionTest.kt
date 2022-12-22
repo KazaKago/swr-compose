60d2a6a2c753f4b8d47b1e65ab1b9ea1d7aecb64
Kensuke Tamura <kazakago@gmail.com>
2022-12-22 15:32:08 +0900

    add InitialSizeTest

---
+++ swr/src/test/java/com/kazakago/swr/compose/infinite/InitialSizeOptionTest.kt
@@ -0,0 +1,67 @@
+package com.kazakago.swr.compose.infinite
+
+import androidx.activity.ComponentActivity
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kazakago.swr.compose.internal.SWRGlobalScope
+import com.kazakago.swr.compose.state.SWRInfiniteState
+import com.kazakago.swr.compose.useSWRInfinite
+import io.kotest.matchers.shouldBe
+import kotlinx.coroutines.delay
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+public class InitialSizeOptionTest {
+
+    @get:Rule
+    public val composeTestRule: AndroidComposeTestRule<ActivityScenarioRule<ComponentActivity>, ComponentActivity> = createAndroidComposeRule()
+
+    @Test
+    public fun initialSize1() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRInfiniteState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            stateList += useSWRInfinite(getKey = { pageIndex, _ -> "${key}_${pageIndex}" }, fetcher = {
+                delay(100)
+                "fetched"
+            }) {
+                initialSize = 1
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(2500)
+        stateList.map { it.data } shouldBe listOf(null, null, listOf("fetched"))
+        stateList.map { it.error } shouldBe listOf(null, null, null)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false)
+    }
+
+    @Test
+    public fun initialSize3() {
+        val key = object {}.javaClass.enclosingMethod?.name
+        val stateList = mutableListOf<SWRInfiniteState<String, String>>()
+        composeTestRule.mainClock.autoAdvance = false
+        composeTestRule.setContent {
+            SWRGlobalScope = rememberCoroutineScope()
+            stateList += useSWRInfinite(getKey = { pageIndex, _ -> "${key}_${pageIndex}" }, fetcher = {
+                delay(100)
+                "fetched"
+            }) {
+                initialSize = 3
+            }
+        }
+
+        composeTestRule.mainClock.advanceTimeBy(2500)
+        stateList.map { it.data } shouldBe listOf(null, null, listOf("fetched", "fetched", "fetched"))
+        stateList.map { it.error } shouldBe listOf(null, null, null)
+        stateList.map { it.isLoading } shouldBe listOf(true, true, false)
+        stateList.map { it.isValidating } shouldBe listOf(false, true, false)
+    }
+}
