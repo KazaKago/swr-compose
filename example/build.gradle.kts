54a0e6132b07cf98d76727d4db7baa1cf232d75a
Kensuke Tamura <kazakago@gmail.com>
2023-04-27 22:17:16 +0900

    update example dependencies

--- example/build.gradle.kts
+++ example/build.gradle.kts
@@ -37,9 +37,9 @@ android {
 
 dependencies {
     implementation(project(":swr"))
-    implementation("androidx.activity:activity-compose:1.7.0")
+    implementation("androidx.activity:activity-compose:1.7.1")
     implementation("androidx.navigation:navigation-compose:2.5.3")
-    implementation(platform("androidx.compose:compose-bom:2023.04.00"))
+    implementation(platform("androidx.compose:compose-bom:2023.04.01"))
     implementation("androidx.compose.material3:material3")
     implementation("com.google.accompanist:accompanist-swiperefresh:0.30.1")
 
