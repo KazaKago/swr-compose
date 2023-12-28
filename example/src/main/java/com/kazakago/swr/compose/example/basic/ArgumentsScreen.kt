5cae7100afeb8ef3c540dba1f2654bbf1399c05d
kazakago <kazakago@gmail.com>
2023-12-28 09:11:18 +0900

    fix example

--- example/src/main/java/com/kazakago/swr/compose/example/basic/ArgumentsScreen.kt
+++ example/src/main/java/com/kazakago/swr/compose/example/basic/ArgumentsScreen.kt
@@ -1,9 +1,18 @@
 package com.kazakago.swr.compose.example.basic
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
--- example/src/main/java/com/kazakago/swr/compose/example/basic/GlobalConfigurationScreen.kt
+++ example/src/main/java/com/kazakago/swr/compose/example/basic/GlobalConfigurationScreen.kt
@@ -7,7 +7,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -17,7 +22,6 @@ import com.kazakago.swr.compose.config.SWRConfig
 import com.kazakago.swr.compose.example.ui.LoadingContent
 import com.kazakago.swr.compose.useSWR
 import kotlinx.coroutines.delay
-import java.util.*
 import kotlin.time.Duration.Companion.seconds
 
 @Composable
